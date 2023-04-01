6d51acbd423dea00e95f62728b627614edc73fb1
league <league@imac-14.attlocal.net>
2023-03-31 18:21:32 -0700

    MAde progress on SMB

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -10,18 +10,24 @@ public static void main(String[] args) {
 		// 1. Save something remarkable about each person in a variable.
 String anthony="Anthony is very skilled";
 String nick="Nick is a frequent Back Market user. He also spends all his money on Nick Graham *trademark sign* shirts and clothing.";
-String osvaldo="Osvaldo is a spy for Russian president Vladimir Putin. :)";
+String mike="Made a 15 year old YT video of him playing Guitar Hero while wearing a banana costume.";
+String osvaldo="Osvaldo is a spy for Russian president Vladimir Putin. He is known to hack people's Google accounts and delete them. He is a fugitive of the San Diego Police Department and is actively being searched for. ";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 String input = JOptionPane.showInputDialog("Enter a name to find out something cool about the person typed.");
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
-if (input.equalsIgnoreCase("anthony")){
-	JOptionPane.showMessageDialog(null,anthony);
+if (input.equalsIgnoreCase("osvaldo")){
+	JOptionPane.showMessageDialog(null,osvaldo);
 }
 if (input.equalsIgnoreCase("nick")){
 	JOptionPane.showMessageDialog(null,nick);
+	
+	if (input.equalsIgnoreCase("mike")){
+		JOptionPane.showMessageDialog(null,mike);
+		
+		
 }
-if (input.equalsIgnoreCase("osvaldo")){
-	JOptionPane.showMessageDialog(null,osvaldo);
+
+
 }
 	}
 }
--- src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
+++ src/_03_if_else/_3_secret_message_box/SecretMessageBox.java
@@ -17,16 +17,33 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		String("passcode");
+	
 	
 	// 1. Set a password in a String variable
 	
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
-
+String JOptionPane.showMessageDialog("Give me a secret message.");
 	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
 	//    if they can guess the passcode
 
 	// 4. If their guess matches the password, show them the secret message
+if Object input;
+input.equals ("passcode");
 
 	// 5. If the password does not match, pop-up "INTRUDER!!"
+if input.equals(password)
+String.JOptionPane.showMessageDialog"INTRUDER!!!!!!!!"
+	}
 
+	private static void showMessageDialog(java.lang.String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void String(String string) {
+		// TODO Auto-generated method stub
+		
+	}
 }
