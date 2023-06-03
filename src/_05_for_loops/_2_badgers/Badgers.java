de8935245aa0f677c879832d29873c4538f5922d
league <league@imac-16.attlocal.net>
2023-06-02 18:25:39 -0700

    Finished both Badgers and My Ages.

---
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -0,0 +1,28 @@
+package _05_for_loops._2_badgers;
+
+public abstract class Badgers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int repeat=0; repeat<2;repeat++)
+		
+		
+		for (int badger=0; badger<12; badger++) {
+			System.out.print("Badger");
+			if(badger <11) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println();
+
+		for (int mushroom=0; mushroom<2; mushroom++) {
+			System.out.print("Mushroom");
+			if(mushroom <1) {
+				System.out.print(", ");
+			}
+		}
+
+		}
+
+	}
+
