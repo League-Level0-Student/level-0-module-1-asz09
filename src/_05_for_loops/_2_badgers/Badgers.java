7ce6a443fea9e67a7009d0829b8084c30474de48
league <league@imac-16.attlocal.net>
2023-06-23 18:19:09 -0700

    actually completed badgers this time, also completed bananas forever.

--- src/_05_for_loops/_2_badgers/Badgers.java
+++ src/_05_for_loops/_2_badgers/Badgers.java
@@ -1,28 +1,33 @@
 package _05_for_loops._2_badgers;
 
+import javax.swing.JOptionPane;
+
 public abstract class Badgers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (int repeat=0; repeat<2;repeat++)
-		
-		
-		for (int badger=0; badger<12; badger++) {
-			System.out.print("Badger");
-			if(badger <11) {
-				System.out.print(", ");
+		for (int repeat=0; repeat<2;repeat++) {
+
+
+			for (int badger=0; badger<12; badger++) {
+				System.out.print("Badger");
+				if(badger <11) {
+					System.out.print(", ");
+				}
 			}
-		}
-		System.out.println();
+			System.out.println();
 
-		for (int mushroom=0; mushroom<2; mushroom++) {
-			System.out.print("Mushroom");
-			if(mushroom <1) {
-				System.out.print(", ");
-			}
+			for (int mushroom=0; mushroom<2; mushroom++) {
+				System.out.print("Mushroom");
+				if(mushroom <1) {
+					System.out.print(", ");
+				}
+			} System.out.println();
+			
 		}
-
-		}
-
+		JOptionPane.showMessageDialog(null, "A SNAKE!!");
 	}
+	
+
+}
 
---
+++ src/_05_for_loops/_3_bananas_4eva/bananas_4eva/bananas_4eva.pde
@@ -0,0 +1,7 @@
+for (int banana=0;banana<100000000000L; banana++){
+
+   System.out.println("Nick Graham \n Osvaldo Ruiz \n Mike Johnson"); 
+    
+    
+  }
+  
